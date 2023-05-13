b3cbec72da55803fd62870280c1ebee638ff54b6
Alex <alex.andrikopoulos@yahoo.com>
2023-05-14 02:55:12 +0300

    lilina Added who I love. Added shopping list.

---
+++ main/lilina/lilina.py
@@ -0,0 +1,13 @@
+print("Hello cats!")
+print("My name is Lilina")
+
+Hello="fuck"
+print(Hello)
+print('hello')
+love="tsib"
+print(f"I {love} my Dudu very much.")
+shopping_list=["chocolate", "cheesecake", "mozzarella", "kinder bueno", "choco milk"]
+print("Bibi buy me everything on the list. Thank you.")
+for shopping_item in shopping_list:
+    print(f"BiBi buy me {shopping_item}")
+
