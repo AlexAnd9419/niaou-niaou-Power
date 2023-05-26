3a6802de777dd247f4720f5d727637242e2e488c
Alex <alex.andrikopoulos@yahoo.com>
2023-05-27 00:16:49 +0300

    Lilila Added Lesson 2 (if)

--- main/lilina/lilina.py
+++ main/lilina/lilina.py
@@ -12,3 +12,10 @@ for shopping_item in shopping_list:
     print(f"BiBi buy me {shopping_item}")
 
 print("lesson 2")
+to_do_today=["meeting", "shopping", "eating"]
+week_days=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+for day in week_days:
+    if day is 'Friday':
+        print(f"On {day} we did a {to_do_today[0]}, then we went {to_do_today[1]}, and finally we went {to_do_today[-1]}")
+
+
